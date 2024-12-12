be05209743aa0df303e94a2e8978eb58b45188a8
Jaimie <jaimie.j.hemmings@gmail.com>
2024-12-12 18:05:56 +0000

    add: individual movie page

--- assets/JS/movies.js
+++ assets/JS/movies.js
@@ -1,7 +1,26 @@
+/** Create global variables .....
+* This is against best practices but for the purpose of this project it is acceptable and likely
+* unavoidable without overly complicating the code and adding unnecessary complexity
+**/
 const globalVars = {
-  currentPage: window.location.pathname
-}
+  currentPage: window.location.pathname.substring(window.location.pathname.lastIndexOf('/')+1),
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+    totalResults: 0,
+  },
+  api: {
+    apiKey: 'a635907d86ee5951aace63e75f36b55a',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  },
+};
 
+/**
+ * Retrieve movie details from the API 
+ * @returns {void}
+ */
 async function displayChristmasMovies() {
   const movieContainer = document.querySelector('.movie-container');
   if (!movieContainer) {
@@ -10,8 +29,7 @@ async function displayChristmasMovies() {
   }
 
   try {
-      const apiKey = 'a635907d86ee5951aace63e75f36b55a';
-      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=christmas`);
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${globalVars.api.apiKey}&query=christmas`);
       
       if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -32,6 +50,7 @@ async function displayChristmasMovies() {
                   <h2>${movie.title}</h2>
                   <p>Release Date: ${movie.release_date}</p>
                   <p>Rating: ${movie.vote_average}</p>
+                  <p><a href="movies/movie.html?id=${movie.id}">View Details</a></p>
               `;
               movieContainer.appendChild(movieCard);
           });
@@ -44,10 +63,75 @@ async function displayChristmasMovies() {
   }
 }
 
+async function retrieveMovieDetails() {
+  const movieId = new URLSearchParams(window.location.search).get('id');
+  if (!movieId) {
+      console.error('Movie ID not found');
+      return;
+  }
+
+  try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${globalVars.api.apiKey}`);
+      
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      const movieDetails = document.querySelector('.movie-details');
+      
+      if (movieDetails) {
+        // set background image for the body
+        document.body.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${data.backdrop_path}')`;
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundPosition = 'center';
+        document.body.style.backgroundAttachment = 'fixed';
+          movieDetails.innerHTML = `
+              <h1 class="text-white">${data.title}</h1>
+              <img src="https://image.tmdb.org/t/p/w500${data.poster_path}"
+                    alt="${data.title}"
+                    onerror="this.src='placeholder.jpg'"/>
+              <p class="text-white">${data.overview}</p>
+              <p class="text-white">Rating: ${data.vote_average}</p>
+              <p class="text-white">Release Date: ${data.release_date}</p>
+              <p class="text-white">Runtime: ${data.runtime} minutes</p>
+              <p class="text-white">Revenue: $${addCommasToNumber(data.revenue)}</p>
+          `;
+      }
+  }
+  catch (error) {
+      console.error('Error fetching movie details:', error);
+  }
+}
+
+/**
+ * Switch to determine current page and call the appropriate function
+ * @returns {void}
+ */
 function init() {
-  console.log('Current page:', globalVars.currentPage); // Debug log
-  if (globalVars.currentPage.includes('movies.html')) {
+  console.log('Current page:', globalVars.currentPage);
+  switch (globalVars.currentPage) {
+    case 'movies.html':
       displayChristmasMovies();
+      break;
+    case 'movie.html':
+      retrieveMovieDetails();
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+ * Add commas to a number
+ * @param {Number} number - The number to add commas to
+ * @returns {String} - The number with commas 
+ **/
+function addCommasToNumber(number) {
+  if(!isNaN(number)) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  } else {
+    return "NaN";
   }
 }
 

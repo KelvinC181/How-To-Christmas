f8ca9585dabca1b8a1de970ce6e29ba04055fd40
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2024-12-14 10:06:05 +0000

    update: lowered music volume

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const audio = document.getElementById("backgroundMusic");
   const muteButton = document.getElementById("muteButton");
+  audio.volume = 0.2;
 
   muteButton.addEventListener("click", () => {
     audio.muted = !audio.muted;

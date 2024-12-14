a3dfae6d2af9149df696168c69ddc93cebb13eb9
Karol S <karol96266@gmail.com>
2024-12-14 10:07:59 +0000

    Add mute functionality in script.js. Add styling to fix mute button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const audio = document.getElementById("backgroundMusic");
   const muteButton = document.getElementById("muteButton");
 
-  muteButton.addEventListener("click", () => {
-    audio.muted = !audio.muted;
+  muteButton.addEventListener("click", (event) => {
+    event.stopPropagation()
+    if (audio.muted) {
+      audio.muted = false;
+      audio.currentTime = 0;
+    } else {
+      audio.muted = true;
+    }
 
     muteButton.textContent = audio.muted ? "Unmute Music" : "Mute Music";
   });
